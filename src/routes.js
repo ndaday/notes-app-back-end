dc83450a9e81dfb322b99c0e786eda4e2fa1bb4a
DDay <nur.andromeda@gmail.com>
2022-07-30 22:50:14 +0700

    menambah hapi

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+const { addNoteHandler } = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  },
+];
+module.exports = routes;
